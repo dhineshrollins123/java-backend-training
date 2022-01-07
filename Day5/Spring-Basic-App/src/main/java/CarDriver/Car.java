0658d718e9d27b2402285d675d967f5024837618
dhinesh.s <dhinesh.s@collabera.com>
2022-01-07 17:55:57 +0530

    UPDATE: SPRING INJECTION

--- Day5/Spring-Basic-App/src/main/java/CarDriver/Car.java
+++ Day5/Spring-Basic-App/src/main/java/CarDriver/Car.java
@@ -2,13 +2,13 @@ package CarDriver;
 
 public class Car {
 
-    private String model;
-    public String getModel() {
-        return model;
+    private int speed;
+    public int getSpeed() {
+        return speed;
     }
 
-    public void setModel(String model) {
-        this.model = model;
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
 
 
--- Day5/Spring-Basic-App/src/main/java/CarDriver/Main.java
+++ Day5/Spring-Basic-App/src/main/java/CarDriver/Main.java
@@ -1,9 +1,15 @@
 package CarDriver;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        Car car=new Car();
-        Driver driver=new Driver();
-        driver.setCar(car);
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Car car1 = (Car) context.getBean("dhinesh");
+        car1.setSpeed(100);
+        Class<Car> cls = Car.class;
+        Car car2 = context.getBean(cls);
+        System.out.println("Car Speed is " + car2.getSpeed());
     }
 }
