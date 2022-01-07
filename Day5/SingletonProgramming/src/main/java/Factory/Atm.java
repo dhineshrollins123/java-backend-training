464737289f678c81e5a77a4a41aae84f61490c72
dhinesh.s <dhinesh.s@collabera.com>
2022-01-07 15:24:58 +0530

    DAY 5 : DESIGN PATTERNS : FACTORY PATTERNS

--- Day5/SingletonProgramming/src/main/java/Factory/Atm.java
+++ Day5/SingletonProgramming/src/main/java/Factory/Atm.java
@@ -6,4 +6,14 @@ public class Atm {
         card.swipe(250);
         return card;
     }
+    public CoinProvider pay(String bank){
+        CoinProvider coin=CoinFactory.getCoin("abc");
+        coin.currentPrice();
+        return coin;
+    }
+    public void usingAppFactory(){
+        AppFactory factory=new AppFactory();
+        CardFactory cardFactory=(CardFactory) factory.getFactory("card");
+        CoinFactory coinFactory=(CoinFactory) factory.getFactory("coin");
+    }
 }
