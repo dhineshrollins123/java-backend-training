0a9bdd9d17946d2032f00880940240cc148b0cb6
dhinesh.s <dhinesh.s@collabera.com>
2022-01-12 01:38:43 +0530

    WEEK 1 Assignment : UPDATED BANKING SYSTEM

--- Assignment/src/main/java/Banksystem/UserAccounts.java
+++ Assignment/src/main/java/Banksystem/UserAccounts.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 
 public class UserAccounts {
     private final Connection connection;
+    private String name;
+    private String email;
+    private long contact;
+    private String password;
+    private long acc_num1;
+    private long acc_num2;
+    private double bal;
+    public boolean isVerification = false;
 
     public UserAccounts(Connection connection) {
         this.connection = connection;
@@ -12,16 +20,102 @@ public class UserAccounts {
 
     Scanner scanner = new Scanner(System.in);
 
-    public int creatingUserAccounts(long account_number, String acc_holder_name, Date acc_creation_date, boolean isActive, double balance, double current_send_account, double current_received_account) throws SQLException {
-        String sql = "insert into employee values(?,?,?,?,?,?,?)";
+    public int signUp() throws SQLException {
+        System.out.println("--------------------------------------");
+        System.out.println("ENTER SIGNUP DETAILS >>>>\n");
+        //scanner.nextLine();
+        System.out.println("Enter Your Name : ");
+        String name = scanner.nextLine();
+        this.name = name;
+        System.out.println("Enter Your Email-ID : ");
+        String email = scanner.next();
+        this.email = email;
+        scanner.nextLine();
+        System.out.println("Enter Your Contact No : ");
+        long cont = scanner.nextLong();
+        String num = Long.toString(cont);
+        if (num.length() == 10) {
+            this.contact = cont;
+            scanner.nextLine();
+            System.out.println("Enter Your Password : ");
+            String pass = scanner.nextLine();
+            this.password = pass;
+            System.out.println("\n");
+            String sql = "insert into admin(name,emailId,contact,password)values(?,?,?,?)";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, this.name);
+            ps.setString(2, this.email);
+            ps.setLong(3, this.contact);
+            ps.setString(4, this.password);
+            int row_affected = ps.executeUpdate();
+            System.out.println("\nYour SignUp Process Has Been Done...\n");
+            System.out.println("\n=====================================");
+            connection.commit();
+            return row_affected;
+        } else {
+            System.out.println("\n Please Enter 10 Digit Mobile Number ^^^\n");
+        }
+        return 0;
+    }
+
+    public int adminLogin() throws SQLException {
+
+        System.out.println("--------------------------------------");
+        System.out.println("ENTER LOGIN DETAILS >>>>\n");
+        System.out.println("Enter Your Mobile Number : ");
+        long con = scanner.nextLong();
+        scanner.nextLine();
+        var sql = "select contact from admin where contact=?";
+        PreparedStatement ps = connection.prepareStatement(sql);
+        // ps.setInt(1, (int) con);
+        ps.setLong(1, con);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            long cont = rs.getLong(1);
+            this.contact = cont;
+        }
+        if (contact != con) {
+            rs.close();
+            connection.rollback();
+            System.out.println("\n\t\tPlease Enter Registered Number...");
+            return 0;
+
+        }
+        System.out.println("Enter Your Password : ");
+        String pwd = scanner.nextLine();
+        var sql1 = "select password from admin where contact=?";
+        PreparedStatement ps1 = connection.prepareStatement(sql1);
+        ps1.setLong(1, contact);
+        ResultSet rs1 = ps1.executeQuery();
+        while (rs1.next()) {
+            String pass = rs1.getString(1);
+            this.password = pass;
+        }
+        if (password.equals(pwd)) {
+            System.out.println(" \n\t\t * * * LOGIN SUCESSFULLY * * *\n");
+            String sql3 = "insert into login values (?,?)";
+            PreparedStatement ps3 = connection.prepareStatement(sql3);
+            ps3.setLong(1, contact);
+            ps3.setString(2, password);
+            int affected1 = ps3.executeUpdate();
+            connection.commit();
+        } else {
+            connection.rollback();
+            System.out.println("\n\t\t Please Enter Registered password...");
+            return 0;
+        }
+        isVerification = true;
+        return 1;
+    }
+
+    public int creatingUserAccounts(long account_number, String acc_holder_name, Date acc_creation_date, boolean isActive, double balance) throws SQLException {
+        String sql = "insert into employee values(?,?,?,?,?)";
         PreparedStatement ps = connection.prepareStatement(sql);
         ps.setLong(1, account_number);
         ps.setString(2, acc_holder_name);
         ps.setDate(3, acc_creation_date);
         ps.setBoolean(4, isActive);
         ps.setDouble(5, balance);
-        ps.setDouble(6, current_send_account);
-        ps.setDouble(7, current_received_account);
         int row_affected = ps.executeUpdate();
         System.out.println("Account Created Sucessfully...\n");
         connection.commit();
@@ -64,67 +158,114 @@ public class UserAccounts {
 
     }
 
-    public int transferMoney(long account_number) throws SQLException {
-        String sql3 = "update employee set balance=balance-4000 where account_number=? ";
-        PreparedStatement ps = connection.prepareStatement(sql3);
-        ps.setDouble(1, account_number);
-        int affected = ps.executeUpdate();
+    public int transferMoney(int a, int b) throws SQLException {
+
+        System.out.println("Enter Your Account Number :");
+        long account_number1 = scanner.nextLong();
+        String sql1 = "select account_number,balance from employee where account_number=?";
+        PreparedStatement ps1 = connection.prepareStatement(sql1);
+        ps1.setLong(1, account_number1);
+        ResultSet rs1 = ps1.executeQuery();
+        while (rs1.next()) {
+            acc_num1 = rs1.getLong(1);
+        }
+        if (account_number1 == acc_num1) {
+            System.out.println("\n\t\tSender Account ID Verified Sucessfully");
+        } else {
+            System.out.println("\n\t\t Please Enter Correct Account Number");
+            return 0;
+        }
+        System.out.println("How Much You Want to Send :");
+        double amount = scanner.nextDouble();
+        while (rs1.next()) {
+            bal = rs1.getDouble("balance");
+        }
+
+        if (amount > bal) {
+            System.out.println("Insufficient Balance...");
+            connection.rollback();
+            return 0;
+        }
+        System.out.println("Enter Receiver Account Number :");
+        long account_number2 = scanner.nextLong();
+        String sql2 = "select account_number from employee where account_number=?";
+        PreparedStatement ps2 = connection.prepareStatement(sql2);
+        ps2.setLong(1, account_number2);
+        ResultSet rs2 = ps2.executeQuery();
+        while (rs2.next()) {
+            acc_num2 = rs2.getLong(1);
+            return 0;
+        }
+        if (account_number2 == acc_num2) {
+            System.out.println("\n\t\tReceiver Account ID Verified Sucessfully");
+        } else {
+            System.out.println("\n\t\t Please Enter Correct Account Number");
+            return 0;
+        }
+        rs2.close();
+
+        String sql3 = "update employee set balance=balance-" + amount + "where account_number=?";
+        PreparedStatement ps3 = connection.prepareStatement(sql3);
+        ps3.setDouble(1, account_number1);
+        int affected1 = ps3.executeUpdate();
+        String sql4 = "update employee set balance=balance+" + amount + "where account_number=? ";
+        PreparedStatement ps4 = connection.prepareStatement(sql4);
+        ps4.setDouble(1, account_number2);
+        int affected2 = ps4.executeUpdate();
         System.out.println("Money Transferred Sucessfully...");
+
         connection.commit();
-        return affected;
+        return 1;
     }
 
-    public void withdrawAmount(long account_number, String acc_holder_name, Date acc_creation_date, boolean isActive, double balance, double currently_send_amount) throws SQLException {
-
-        var sql1 = "insert into employee values(?,?,?,?,?,?,?)";
-        var ps = connection.prepareStatement(sql1);
-        ps.setLong(1, account_number);
-        ps.setString(2, acc_holder_name);
-        ps.setDate(3, acc_creation_date);
-        ps.setBoolean(4, isActive);
-        ps.setDouble(5, balance);
-        ps.setDouble(6, currently_send_amount);
-        double currently_received_amount = 0;
-        ps.setDouble(7, currently_received_amount);
-        var affect1 = ps.executeUpdate();
-        connection.commit();
-        var sql2 = "update employee set balance=balance-currently_send_amount where account_number=?";
-        PreparedStatement ps1 = connection.prepareStatement(sql2);
+    public void withdrawAmount(long account_num, double amount1) throws SQLException {
+        System.out.println("Enter Your Account Number :");
+        long account_number = scanner.nextLong();
+        String sql1 = "select account_number,balance from employee where account_number=?";
+        PreparedStatement ps1 = connection.prepareStatement(sql1);
         ps1.setLong(1, account_number);
-
-        var affect2 = ps1.executeUpdate();
+        ResultSet rs1 = ps1.executeQuery();
+        while (rs1.next()) {
+            acc_num1 = rs1.getLong(1);
+        }
+        System.out.println("Enter Withdrawl Amount :");
+        double amount = scanner.nextDouble();
+        var sql2 = "update employee set balance=balance-" + amount + "where account_number=?";
+        PreparedStatement ps2 = connection.prepareStatement(sql2);
+        ps2.setLong(1, account_number);
+        var affect2 = ps2.executeUpdate();
         if (affect2 == 0) {
             connection.rollback();
-            System.out.println("Transaction Failed");
+            System.out.println("....Transaction Failed....");
         } else {
             connection.commit();
-            System.out.println("Transaction Completed Sucessfully...");
+            System.out.println("\n\t\t*** Transaction Completed Sucessfully...***");
         }
     }
 
-    public void depositAmount(long account_number, String acc_holder_name, Date acc_creation_date, boolean isActive, double balance, double currently_received_amount) throws SQLException {
-        var sql1 = "insert into employee values(?,?,?,?,?,?,?)";
-        var ps = connection.prepareStatement(sql1);
-        ps.setLong(1, account_number);
-        ps.setString(2, acc_holder_name);
-        ps.setDate(3, acc_creation_date);
-        ps.setBoolean(4, isActive);
-        ps.setDouble(5, balance);
-        double currently_send_amount = 0;
-        ps.setDouble(6, currently_send_amount);
-        ps.setDouble(7, currently_received_amount);
-        var affect1 = ps.executeUpdate();
-        connection.commit();
-        var sql2 = "update employee set balance=balance+currently_received_amount where account_number=?";
-        PreparedStatement ps1 = connection.prepareStatement(sql2);
+    public void depositAmount(long account_num, double amoun) throws SQLException {
+        System.out.println("Enter Account Number :");
+        long account_number = scanner.nextLong();
+        String sql1 = "select account_number,balance from employee where account_number=?";
+        PreparedStatement ps1 = connection.prepareStatement(sql1);
         ps1.setLong(1, account_number);
-        var affect2 = ps1.executeUpdate();
+        ResultSet rs1 = ps1.executeQuery();
+        while (rs1.next()) {
+            acc_num1 = rs1.getLong(1);
+        }
+        System.out.println("Enter Deposit Amount :");
+        double amount = scanner.nextDouble();
+        var sql2 = "update employee set balance=balance+" + amount + "where account_number=?";
+        PreparedStatement ps2 = connection.prepareStatement(sql2);
+
+        ps2.setLong(1, account_number);
+        var affect2 = ps2.executeUpdate();
         if (affect2 == 0) {
             connection.rollback();
             System.out.println("Transaction Failed");
         } else {
             connection.commit();
-            System.out.println("Transaction Completed Sucessfully...");
+            System.out.println("\n\t\t*** Transaction Completed Sucessfully...***");
         }
     }
 
@@ -139,7 +280,10 @@ public class UserAccounts {
             boolean isActive = result.getBoolean(2);
             System.out.println("Account Number : " + acc_num);
             System.out.println("Account Status : " + isActive);
+
         }
         result.close();
     }
+
+
 }
--- Assignment/src/main/java/Main.java
+++ Assignment/src/main/java/Main.java
@@ -1,26 +1,23 @@
 import Banksystem.UserAccounts;
 import connectivity.Connectivity;
 
-import java.sql.Date;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
+
     static Scanner scanner = new Scanner(System.in);
-    private String name;
-    private String email;
-    private long contact;
-    private String password;
-    boolean isVerification = false;
+    public Main() throws SQLException, ClassNotFoundException {
+
+    }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        Connectivity connectivity = new Connectivity();
-        UserAccounts reference = new UserAccounts(connectivity.getConnection());
-        Main main = new Main();
         boolean isTimer = true;
-
+        Connectivity connectivity = new Connectivity();
+        Main main = new Main();
+        UserAccounts reference = new UserAccounts(connectivity.getConnection());
         System.out.println("\t\t====### Welcome To Banking System ###====\n");
         while (isTimer == true) {
             System.out.println(" \n\t* Select the Options Given Below \n");
@@ -28,64 +25,73 @@ public class Main {
             System.out.println("\t ENTER 2 : ADMIN LOGIN \n");
             System.out.println("\t ENTER 3 : CREATE ACCOUNTS \n");
             System.out.println("\t ENTER 4 : SHOW ALL ACCOUNTS \n");
-            System.out.println("\t ENTER 5 : TRANSFER MONEY \n");
-            System.out.println("\t ENTER 6 : FOR WITHDRAWL \n");
-            System.out.println("\t ENTER 7 : FOR DEPOSIT \n");
-            System.out.println("\t ENTER 8 : ACCOUNT STATUS CHECKING \n");
-            System.out.println("\t ENTER 9 : EXIT\n");
+           // System.out.println("\t ENTER 5 : TRANSFER MONEY \n");
+            System.out.println("\t ENTER 5 : FOR WITHDRAWL \n");
+            System.out.println("\t ENTER 6 : FOR DEPOSIT \n");
+            System.out.println("\t ENTER 7 : ACCOUNT STATUS CHECKING \n");
+            System.out.println("\t ENTER 8 : EXIT\n");
             int option = scanner.nextInt();
 
             switch (option) {
                 case 1:
                     System.out.println("\n YOU SELECTED OPTION :" + option + "\n");
-                    main.signUp();
+                    reference.signUp();
                     break;
                 case 2:
                     System.out.println("\n YOU SELECTED OPTION :" + option + "\n");
-                    main.adminLogin();
+                    reference.adminLogin();
                     break;
                 case 3:
                     System.out.println("\n YOU SELECTED OPTION :" + option + "\n");
-                    if (main.isVerification) {
-                        reference.creatingUserAccounts(434456789123L, "Likhitha", Date.valueOf(LocalDate.now()), true, 3000, 0, 0);
+                    if (reference.isVerification) {
+                        System.out.println("\nEnter Account Number : ");
+                        long account_num=scanner.nextLong();
+                        scanner.nextLine();
+                        System.out.println("\nEnter Account Holder Name : ");
+                        String account_hol_name= scanner.nextLine();
+                        System.out.println("\nEnter Current Balance : ");
+                        double balance= scanner.nextDouble();
+                        reference.creatingUserAccounts(account_num,account_hol_name, Date.valueOf(LocalDate.now()), true,balance);
                     } else System.out.println("Please Login After Come Here...");
                     break;
                 case 4:
-                    if (main.isVerification) {
+                    if (reference.isVerification) {
                         System.out.println("\n YOU SELECTED OPTION :" + option + "\n");
                     } else System.out.println("Please Login After Come Here...");
                     reference.displayAllAccounts();
                     break;
+              /*  case 5:
+                   // if (main.isVerification) {
+                    System.out.println("\n YOU SELECTED OPTION :" + option + "\n");
+                        reference.transferMoney(123,456);
+                  //  } else System.out.println("Please Login After Come Here...");
+
+                    break;*/
+
                 case 5:
-                    if (main.isVerification) {
+                    if (reference.isVerification) {
                         System.out.println("\n YOU SELECTED OPTION :" + option + "\n");
-                        reference.transferMoney(781456789126L);
+                        reference.withdrawAmount(781456789126L,0);
                     } else System.out.println("Please Login After Come Here...");
 
                     break;
-
                 case 6:
-                    if (main.isVerification) {
+                    if (reference.isVerification) {
                         System.out.println("\n YOU SELECTED OPTION :" + option + "\n");
-                        reference.withdrawAmount(781456789126L, "raja", Date.valueOf(LocalDate.now()), false, 10000, 3000);
+                        reference.depositAmount(356456789126L,0);
                     } else System.out.println("Please Login After Come Here...");
 
                     break;
                 case 7:
-                    if (main.isVerification) {
+                    if (reference.isVerification) {
                         System.out.println("\n YOU SELECTED OPTION :" + option + "\n");
-                        reference.depositAmount(356456789126L, "karthik", Date.valueOf(LocalDate.now()), false, 10000, 3000);
+                        System.out.println("\n Enter Account Number For Status Checking : ");
+                        long status= scanner.nextLong();
+                        reference.accountStatusChecking(status);
                     } else System.out.println("Please Login After Come Here...");
 
                     break;
                 case 8:
-                    if (main.isVerification) {
-                        System.out.println("\n YOU SELECTED OPTION :" + option + "\n");
-                        reference.accountStatusChecking(789456789123L);
-                    } else System.out.println("Please Login After Come Here...");
-
-                    break;
-                case 9:
                     System.out.println("\n YOU SELECTED OPTION :" + option + "\n");
                     System.out.println("\n\t** THANKS FOR VISITING US...COME AGAIN WELCOME...**");
                     isTimer = false;
@@ -99,55 +105,9 @@ public class Main {
     }
 
 
-    public void signUp() {
-        scanner.nextLine();
-        System.out.println("--------------------------------------");
-        System.out.println("ENTER SIGNUP DETAILS >>>>\n");
-        System.out.println("Enter Your Name : ");
-        String name = scanner.nextLine();
-        this.name = name;
-        System.out.println("Enter Your Email-ID : ");
-        String email = scanner.next();
-        this.email = email;
-        scanner.nextLine();
-        System.out.println("Enter Your Contact No : ");
-        long cont = scanner.nextLong();
-        String num = Long.toString(cont);
-        if (num.length() == 10) {
-            this.contact = cont;
-            scanner.nextLine();
-            System.out.println("Enter Your Password : ");
-            String pass = scanner.nextLine();
-            this.password = pass;
-            System.out.println("\n");
-            System.out.println("Your SignUp Process Has Been Done...");
-            System.out.println("\n=====================================");
-        } else {
-            System.out.println("\n Please Enter 10 Digit Mobile Number ^^^\n");
-        }
 
 
-    }
 
 
-    public void adminLogin() {
 
-        System.out.println("--------------------------------------");
-        System.out.println("ENTER LOGIN DETAILS >>>>\n");
-        System.out.println("Enter Your Mobile Number : ");
-        long con = scanner.nextLong();
-        scanner.nextLine();
-        System.out.println("Enter Your Password : ");
-        String pwd = scanner.nextLine();
-        if (this.contact == con) {
-            if (this.password.equals(pwd)) {
-                System.out.println(" \n * LOGIN SUCESSFULLY *\n");
-                isVerification = true;
-            } else {
-                System.out.println("\n OOPS ! ....Please Enter Correct Password....\n");
-            }
-        } else {
-            System.out.println("\n OOPS ! ...Please Enter Correct Mobile Number...or Do SIGN UP...\n");
-        }
-    }
 }
