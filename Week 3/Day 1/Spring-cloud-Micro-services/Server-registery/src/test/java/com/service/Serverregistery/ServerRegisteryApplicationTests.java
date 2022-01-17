3d29bdb53820354afc2475999c4f893e1b7ed4c5
dhinesh.s <dhinesh.s@collabera.com>
2022-01-17 13:39:53 +0530

    UPDATED SERVICE REGISTERY

---
+++ Week 3/Day 1/Spring-cloud-Micro-services/Server-registery/src/test/java/com/service/Serverregistery/ServerRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.Serverregistery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
