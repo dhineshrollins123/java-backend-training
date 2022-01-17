ea4662a5ac501decd274ce75aeabfeb95ebb3f1c
dhinesh.s <dhinesh.s@collabera.com>
2022-01-17 18:13:05 +0530

    UPDATED ABC FILE MICROSERVICE

--- Week 3/Day 1/Spring-cloud-Micro-services/abc-microservice/src/main/java/com/service/abcmicroservice/controller/AbcController.java
+++ Week 3/Day 1/Spring-cloud-Micro-services/abc-microservice/src/main/java/com/service/abcmicroservice/controller/AbcController.java
@@ -1,15 +1,52 @@
 package com.service.abcmicroservice.controller;
 
 
+import com.service.abcmicroservice.dto.Pqr;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequestMapping("/api/abc")
 @RestController
 public class AbcController {
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping
-    public ResponseEntity<String> get(){
+    public ResponseEntity<String> get() {
         return ResponseEntity.ok("Iam From ABC CONTROLLER");
+    }
+
+    @GetMapping("/{st}/{ed}")
+    public ResponseEntity<List<String>> postInAbc(
+            @PathVariable String st,
+            @PathVariable String ed
+    ) {
+        var lcSt = LocalDate.parse(st);
+        var lcEd = LocalDate.parse(ed);
+        ResponseEntity<List<Pqr>> entity = restTemplate.exchange(
+                        "http://localhost:8080/api/pqr/" + lcSt.toString() + "/" + lcEd.toString(),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Pqr>>() {
+                }
+
+        );
+        List<String> names = entity.getBody()
+                .stream()
+                .map(p -> p.getName())
+                .collect(Collectors.toList()
+                );
+        return ResponseEntity.ok(names);
     }
 }
